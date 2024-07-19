f01e68d6ea898cc540ab30687f668077dd8a81be
Matthew Zhou <phoisgood1@gmail.com>
2024-07-19 13:56:27 -0700

    changed the constructor to be more robust and formatted with black

--- receipt_processing_app/controller/request_handler.py
+++ receipt_processing_app/controller/request_handler.py
@@ -3,25 +3,22 @@ from typing import Dict
 from receipt_processing_app.model.receipt import Receipt
 from receipt_processing_app.model.item import Item
 from receipt_processing_app.controller.in_memory_database_controller import (
-    InMemoryDatabaseController,
-)
-from receipt_processing_app.controller.receipt_processing_service import (
-    ReceiptProcessingService,
+    DataController,
 )
 
 
 class RequestHandler:
-    def __init__(self):
-        self.in_memory_database_controller = InMemoryDatabaseController()
-        self.receipt_processing_service = ReceiptProcessingService()
+    def __init__(self, data_controller: DataController, receipt_processing_service):
+        self.data_controller = data_controller
+        self.receipt_processing_service = receipt_processing_service
 
     def process_receipts(self, receipt: Dict) -> str:
         receipt["items"] = [Item(**item) for item in receipt["items"]]
         receipt = Receipt(**receipt)
-        receipt_id = self.in_memory_database_controller.create_receipt(receipt)
+        receipt_id = self.data_controller.create_receipt(receipt)
         return receipt_id
 
-    def get_points(self, receiptId: str) -> int:
-        receipt = self.in_memory_database_controller.get_receipt(receiptId)
+    def get_points(self, receipt_id: str) -> int:
+        receipt = self.data_controller.get_receipt(receipt_id)
         total_points = self.receipt_processing_service.calculate_points(receipt)
         return total_points
--- receipt_processing_app/tests/test_request_handler.py
+++ receipt_processing_app/tests/test_request_handler.py
@@ -1,5 +1,11 @@
 import unittest
 from receipt_processing_app.controller.request_handler import RequestHandler
+from receipt_processing_app.controller.receipt_processing_service import (
+    ReceiptProcessingService,
+)
+from receipt_processing_app.controller.in_memory_database_controller import (
+    InMemoryDatabaseController,
+)
 
 
 class TestRequestHandler(unittest.TestCase):
@@ -18,17 +24,29 @@ class TestRequestHandler(unittest.TestCase):
             ],
             "total": "35.35",
         }
-        request_handler = RequestHandler()
+        in_memory_database_controller = InMemoryDatabaseController()
+        receipt_processing_service = ReceiptProcessingService()
+        request_handler = RequestHandler(
+            in_memory_database_controller, receipt_processing_service
+        )
         receipt_id = request_handler.process_receipts(receipt)
         total_points = request_handler.get_points(receipt_id)
         self.assertEqual(total_points, 28)
 
     def test_get_points_with_empty_id(self):
-        requestHandler = RequestHandler()
+        in_memory_database_controller = InMemoryDatabaseController()
+        receipt_processing_service = ReceiptProcessingService()
+        request_handler = RequestHandler(
+            in_memory_database_controller, receipt_processing_service
+        )
         with self.assertRaises(ValueError):
-            requestHandler.get_points("")
+            request_handler.get_points("")
 
     def test_get_points_with_nonexistent_id(self):
-        requestHandler = RequestHandler()
+        in_memory_database_controller = InMemoryDatabaseController()
+        receipt_processing_service = ReceiptProcessingService()
+        request_handler = RequestHandler(
+            in_memory_database_controller, receipt_processing_service
+        )
         with self.assertRaises(ValueError):
-            requestHandler.get_points("this should not exist")
+            request_handler.get_points("this should not exist")
